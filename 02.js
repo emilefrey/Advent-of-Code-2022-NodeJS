8e6ee2ae95ce4680b81c07c4b2a0b383572dbeda
Emile Frey <ewf003@gmail.com>
2022-12-03 01:04:16 -0600

    day 2 and 3

--- 02.js
+++ 02.js
@@ -1,11 +1,109 @@
 var helpers = require("./helpers");
-
+const shapeScores = { X: 1, Y: 2, Z: 3 };
 const part1 = (data) => {
-  return "TODO";
+  const rounds = helpers.splitByNewLine(data);
+  const determineIfWon = (shapeOfOpponent, shapeOfPersonInQuestion) => {
+    switch (shapeOfPersonInQuestion) {
+      // if rock
+      case "X":
+        // can only win if opponent threw scissors
+        return shapeOfOpponent === "C";
+      // if paper
+      case "Y":
+        return shapeOfOpponent === "A";
+      // if scissors
+      case "Z":
+        return shapeOfOpponent === "B";
+    }
+  };
+
+  const determineRoundScore = (opponent, me) => {
+    let myScore = 0;
+    // if tie
+    if (
+      (opponent === "A" && me === "X") ||
+      (opponent === "B" && me === "Y") ||
+      (opponent === "C" && me === "Z")
+    ) {
+      myScore += 3;
+      // add 6 points if winner
+    } else {
+      myScore += determineIfWon(opponent, me) ? 6 : 0;
+    }
+    // add shape score
+    myScore += shapeScores[me];
+    return myScore;
+  };
+
+  let finalScore = 0;
+  rounds.forEach((round) => {
+    const [opponent, me] = round.split(" ");
+    finalScore += determineRoundScore(opponent, me);
+  });
+
+  return finalScore;
 };
 
 const part2 = (data) => {
-  return "TODO";
+  const rounds = helpers.splitByNewLine(data);
+
+  const determineIfWon = (shapeOfOpponent, moveToMake) => {
+    let shapeToPick = "";
+    let winStatus = false;
+    switch (moveToMake) {
+      // lose
+      case "X":
+        if (shapeOfOpponent === "A") {
+          shapeToPick = "Z";
+        } else if (shapeOfOpponent === "B") {
+          shapeToPick = "X";
+        } else if (shapeOfOpponent === "C") {
+          shapeToPick = "Y";
+        }
+        break;
+      case "Y":
+        if (shapeOfOpponent === "A") {
+          shapeToPick = "X";
+        } else if (shapeOfOpponent === "B") {
+          shapeToPick = "Y";
+        } else if (shapeOfOpponent === "C") {
+          shapeToPick = "Z";
+        }
+        break;
+      case "Z":
+        if (shapeOfOpponent === "A") {
+          shapeToPick = "Y";
+        } else if (shapeOfOpponent === "B") {
+          shapeToPick = "Z";
+        } else if (shapeOfOpponent === "C") {
+          shapeToPick = "X";
+        }
+        winStatus = true;
+        break;
+    }
+    return { didIWin: winStatus, shapeToPick: shapeToPick };
+  };
+
+  const determineScore = (opponent, me) => {
+    let myScore = 0;
+    // if tie
+    const { didIWin, shapeToPick } = determineIfWon(opponent, me);
+    if (me === "Y") {
+      myScore += 3;
+    } else {
+      myScore += didIWin ? 6 : 0;
+    }
+    myScore += shapeScores[shapeToPick];
+    return myScore;
+  };
+
+  let finalScore = 0;
+  rounds.forEach((round) => {
+    const [opponent, me] = round.split(" ");
+    finalScore += determineScore(opponent, me);
+  });
+
+  return finalScore;
 };
 
 module.exports = {
---
+++ 03.js
@@ -0,0 +1,38 @@
+var helpers = require("./helpers");
+const isUpperCase = (string) => /^[A-Z]*$/.test(string);
+
+const part1 = (data) => {
+  const sacks = helpers.splitByNewLine(data);
+  let total = 0;
+  sacks.forEach((sack) => {
+    const sackCompartLength = sack.length / 2;
+    const comp1 = [...sack.slice(0, sackCompartLength)];
+    const comp2 = [...sack.slice(sackCompartLength * -1)];
+    matching = comp1.filter((comp1item) => comp2.includes(comp1item))[0];
+    total += isUpperCase(matching)
+      ? matching.charCodeAt(0) - 38
+      : matching.charCodeAt(0) - 96;
+  });
+  return total;
+};
+
+const part2 = (data) => {
+  const sacks = helpers.splitByNewLine(data);
+  let total = 0;
+  for (let i = 0; i < sacks.length; i += 3) {
+    const matching = [...sacks[i]].filter(
+      (item) =>
+        [...sacks[i + 1]].includes(item) && [...sacks[i + 2]].includes(item)
+    )[0];
+    total += isUpperCase(matching)
+      ? matching.charCodeAt(0) - 38
+      : matching.charCodeAt(0) - 96;
+  }
+
+  return total;
+};
+
+module.exports = {
+  part1,
+  part2,
+};
