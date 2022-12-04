4cfc408b6dcb3ae7c9a05a8d246955a02d7a4a6f
Emile Frey <ewf003@gmail.com>
2022-12-04 09:32:02 -0600

    getting ready for day 5

---
+++ 05.js
@@ -0,0 +1,22 @@
+var helpers = require("./helpers");
+
+const part1 = (data) => {
+  const stuff = helpers.splitByNewLine(data);
+  let total = 0;
+
+  stuff.forEach((a) => {});
+  return total;
+};
+
+const part2 = (data) => {
+  const stuff = helpers.splitByNewLine(data);
+  let total = 0;
+
+  stuff.forEach((a) => {});
+  return total;
+};
+
+module.exports = {
+  part1,
+  part2,
+};
