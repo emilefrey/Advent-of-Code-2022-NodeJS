08ef82bc851efcd5ac3e354f3b0e2154c7496ac7
Emile Frey <efrey@magentatech.io>
2022-12-05 11:55:13 -0600

    updated day 5 code to process data

--- 05.js
+++ 05.js
@@ -1,76 +1,70 @@
 var helpers = require("./helpers");
 
 const part1 = (data) => {
-  const [_, instructions] = helpers.splitByBlankLine(data);
-  const stack1 = ["B", "P", "N", "Q", "H", "D", "R", "T"];
-  const stack2 = ["W", "G", "B", "J", "T", "V"];
-  const stack3 = ["N", "R", "H", "D", "S", "V", "M", "Q"];
-  const stack4 = ["P", "Z", "N", "M", "C"];
-  const stack5 = ["D", "Z", "B"];
-  const stack6 = ["V", "C", "W", "Z"];
-  const stack7 = ["G", "Z", "N", "C", "V", "Q", "L", "S"];
-  const stack8 = ["L", "G", "J", "M", "D", "N", "V"];
-  const stack9 = ["T", "P", "M", "F", "Z", "C", "G"];
+  const [stackData, instructions] = helpers.splitByBlankLine(data);
+  const finalStackArray = getArrayOfStackArrays(stackData);
 
-  const arrayOfStacks = [
-    stack1,
-    stack2,
-    stack3,
-    stack4,
-    stack5,
-    stack6,
-    stack7,
-    stack8,
-    stack9,
-  ];
   helpers.splitByNewLine(instructions).forEach((step) => {
     const [_, howMany, _2, from, _3, to] = step.split(" ").map(Number);
     for (let i = 0; i < howMany; i++) {
-      const removed = arrayOfStacks[from - 1].pop();
-      arrayOfStacks[to - 1].push(removed);
+      const removed = finalStackArray[from - 1].pop();
+      finalStackArray[to - 1].push(removed);
     }
   });
-  const topOfStacks = arrayOfStacks.map((stack) => stack[stack.length - 1]);
+  const topOfStacks = finalStackArray.map((stack) => stack[stack.length - 1]);
 
   return topOfStacks;
 };
 
 const part2 = (data) => {
-  const [_, instructions] = helpers.splitByBlankLine(data);
-  const stack1 = ["B", "P", "N", "Q", "H", "D", "R", "T"];
-  const stack2 = ["W", "G", "B", "J", "T", "V"];
-  const stack3 = ["N", "R", "H", "D", "S", "V", "M", "Q"];
-  const stack4 = ["P", "Z", "N", "M", "C"];
-  const stack5 = ["D", "Z", "B"];
-  const stack6 = ["V", "C", "W", "Z"];
-  const stack7 = ["G", "Z", "N", "C", "V", "Q", "L", "S"];
-  const stack8 = ["L", "G", "J", "M", "D", "N", "V"];
-  const stack9 = ["T", "P", "M", "F", "Z", "C", "G"];
+  const [stackData, instructions] = helpers.splitByBlankLine(data);
+  const finalStackArray = getArrayOfStackArrays(stackData);
 
-  const arrayOfStacks = [
-    stack1,
-    stack2,
-    stack3,
-    stack4,
-    stack5,
-    stack6,
-    stack7,
-    stack8,
-    stack9,
-  ];
   helpers.splitByNewLine(instructions).forEach((step) => {
     const [_, howMany, _2, from, _3, to] = step.split(" ").map(Number);
     const removed = [];
     for (let i = 0; i < howMany; i++) {
-      const popped = arrayOfStacks[from - 1].pop();
+      const popped = finalStackArray[from - 1].pop();
       removed.unshift(popped);
     }
-    arrayOfStacks[to - 1] = [...arrayOfStacks[to - 1], ...removed];
+    finalStackArray[to - 1] = [...finalStackArray[to - 1], ...removed];
   });
 
-  const topOfStacks = arrayOfStacks.map((stack) => stack[stack.length - 1]);
+  const topOfStacks = finalStackArray.map((stack) => stack[stack.length - 1]);
 
   return topOfStacks;
+};
+
+// i hate this code, but just kept moving forward trying to process the stack data into array of stack arrays
+const getArrayOfStackArrays = (stackData) => {
+  const stackDataArray = helpers.splitByNewLine(stackData);
+  let line = 0;
+  const stacks = [];
+  while (stackDataArray[line].includes("[")) {
+    const stackLine = stackDataArray[line];
+    const currentStack = [];
+    for (
+      let stackCharacterIndex = 1;
+      stackCharacterIndex < stackLine.length;
+      stackCharacterIndex += 4
+    ) {
+      const item = stackLine[stackCharacterIndex];
+      currentStack.push(item);
+    }
+    stacks.push(currentStack);
+    line += 1;
+  }
+
+  let finalStackArray = [];
+  for (let arrays = 0; arrays < 9; arrays += 1) {
+    let finalStack = [];
+    for (let stackArrayIndex = 7; stackArrayIndex >= 0; stackArrayIndex--) {
+      const toPush = stacks[stackArrayIndex].shift();
+      if (toPush !== " ") finalStack.push(toPush);
+    }
+    finalStackArray.push(finalStack);
+  }
+  return finalStackArray;
 };
 
 module.exports = {
