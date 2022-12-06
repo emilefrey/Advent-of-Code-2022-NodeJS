3318a0820c637dac325d8207bb3df40a7bff5ec2
Emile Frey <ewf003@gmail.com>
2022-12-05 23:57:46 -0600

    renaming function/removing import

--- 06.js
+++ 06.js
@@ -1,21 +1,18 @@
-var helpers = require("./helpers");
-
-const getSubstringWithUniqueCharactersPosition = (input, count) => {
+const getMarker = (input, count) => {
   for (let i = 0; i < input.length; i++) {
     const set = new Set(input.substr(i, count));
     if (set.size === count) {
       return i + count;
     }
   }
-  return `No substring with unique characters of length ${count}!`;
 };
 
 const part1 = (data) => {
-  return getSubstringWithUniqueCharactersPosition(data, 4);
+  return getMarker(data, 4);
 };
 
 const part2 = (data) => {
-  return getSubstringWithUniqueCharactersPosition(data, 14);
+  return getMarker(data, 14);
 };
 
 module.exports = {
