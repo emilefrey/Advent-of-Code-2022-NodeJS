e939391fc8fec2286f42b7eb0a91a7c032d0b857
Emile Frey <ewf003@gmail.com>
2022-12-08 22:58:03 -0600

    day 9 prep

---
+++ 09.js
@@ -0,0 +1,18 @@
+var helpers = require("./helpers");
+
+const part1 = (data) => {
+  const stuff = helpers.splitByNewLine(data);
+  stuff.forEach((item) => {});
+  return 0;
+};
+
+const part2 = (data) => {
+  const stuff = helpers.splitByNewLine(data);
+  stuff.forEach((item) => {});
+  return 0;
+};
+
+module.exports = {
+  part1,
+  part2,
+};
