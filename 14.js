41c55570e98810cf0957155bf27aaf9d01254fc4
Emile Frey <ewf003@gmail.com>
2022-12-13 23:51:28 -0600

    14 start like crap

--- 14.js
+++ 14.js
@@ -1,7 +1,41 @@
 var helpers = require("./helpers");
 
 const part1 = (data) => {
-  return 0;
+  const walls = helpers.splitByNewLine(data);
+  const arrayOfWalls = [];
+  walls.forEach((wall) => {
+    const coordinates = wall.split(" -> ");
+    for (
+      let coordinate = 0;
+      coordinate < coordinates.length - 1;
+      coordinate++
+    ) {
+      const [x1, y1] = coordinates[coordinate].split(",")?.map(Number);
+      const [x2, y2] = coordinates[coordinate + 1].split(",")?.map(Number);
+      if (x1 === x2) {
+        if (y2 - y1 > 0) {
+          for (let i = y1; i <= y2; i++) {
+            arrayOfWalls.push(`${x1},${i}`);
+          }
+        } else {
+          for (let i = y2; i <= y1; i++) {
+            arrayOfWalls.push(`${x1},${i}`);
+          }
+        }
+      } else if (y1 === y2) {
+        if (x2 - x1 > 0) {
+          for (let i = x1; i <= x2; i++) {
+            arrayOfWalls.push(`${i},${y1}`);
+          }
+        } else {
+          for (let i = x2; i <= x1; i++) {
+            arrayOfWalls.push(`${i},${y1}`);
+          }
+        }
+      }
+    }
+  });
+  return arrayOfWalls;
 };
 
 const part2 = (data) => {
