8e98606162713fb7839293cf36be7dfc4a673d2c
Emile Frey <efrey@magentatech.io>
2022-12-16 18:45:11 -0600

    15 Part 2 passing - PITA

--- 15.js
+++ 15.js
@@ -54,8 +54,107 @@ const part1 = (data) => {
   return row.length;
 };
 
+const inRange = (value, max) => {
+  return value >= 0 && value <= max;
+};
+
+const coordinatesInRange = (xValue, yValue, max) => {
+  return inRange(xValue, max) && inRange(yValue, max);
+};
+
+const storeIfInRange = (xValue, yValue, array, max) => {
+  if (coordinatesInRange(xValue, yValue, max)) {
+    array.push({ x: xValue, y: yValue });
+  }
+};
+
+const determineIfPointIsWithinOtherSensorsRange = (point, sensorArray) => {
+  let inRange = false;
+
+  // check to see if at least one sensor can "see" point
+  // if not, we know we have the point we're interested in and should return it
+  // otherwise, return true
+
+  for (let i = 0; i < sensorArray.length; i++) {
+    if (
+      Math.abs(point.x - sensorArray[i].sensorX) +
+        Math.abs(point.y - sensorArray[i].sensorY) <=
+      sensorArray[i].manhattanDistance
+    ) {
+      inRange = true;
+      break;
+    }
+  }
+  return { inRange: inRange, x: point.x, y: point.y };
+};
+
 const part2 = (data) => {
-  return 0;
+  const sensorBeaconData = helpers
+    .splitByNewLine(data)
+    .map((line) =>
+      line
+        .replace("Sensor at ", "")
+        .replace("closest beacon is at ", "")
+        .replaceAll("x=", "")
+        .replaceAll("y=", "")
+    )
+    .map((item) => item.split(": "));
+  const max = 4000000;
+  const sensors = {};
+  sensorBeaconData.forEach((pair, index) => {
+    const [sensorX, sensorY] = pair[0].split(",").map(Number);
+    const [beaconX, beaconY] = pair[1].split(",").map(Number);
+    const manhattanDistance =
+      Math.abs(beaconY - sensorY) + Math.abs(beaconX - sensorX);
+    const pointsOutside = [];
+    for (let i = 0; i <= manhattanDistance + 1; i++) {
+      const yPlus = sensorY + i;
+      const yMinus = sensorY - i;
+      const xPlus = sensorX + (manhattanDistance + 1 - i);
+      const xMinus = sensorX - (manhattanDistance + 1 - i);
+      storeIfInRange(xPlus, yPlus, pointsOutside, max);
+      storeIfInRange(xPlus, yMinus, pointsOutside, max);
+      storeIfInRange(xMinus, yPlus, pointsOutside, max);
+      storeIfInRange(xMinus, yMinus, pointsOutside, max);
+    }
+    sensors[index] = {
+      sensorX: sensorX,
+      sensorY: sensorY,
+      manhattanDistance: manhattanDistance,
+      pointsOutside: pointsOutside,
+    };
+  });
+
+  const sensorArray = Object.values(sensors);
+  let uncoveredX;
+  let uncoveredY;
+  let pointFound = false;
+  for (let sensorIndex = 0; sensorIndex < sensorArray.length; sensorIndex++) {
+    for (
+      let pointIndex = 0;
+      pointIndex < sensorArray[sensorIndex].pointsOutside.length;
+      pointIndex++
+    ) {
+      const { inRange, x, y } = determineIfPointIsWithinOtherSensorsRange(
+        sensorArray[sensorIndex].pointsOutside[pointIndex],
+        sensorArray
+      );
+      if (inRange) {
+        continue;
+      } else {
+        pointFound = true;
+        uncoveredX = x;
+        uncoveredY = y;
+        break;
+      }
+    }
+    if (!pointFound) {
+      continue;
+    } else {
+      break;
+    }
+  }
+  return uncoveredX * 4000000 + uncoveredY;
 };
 
 module.exports = {
